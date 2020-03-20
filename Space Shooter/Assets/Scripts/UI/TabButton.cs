27633bf45158ece7313fcf6e928c5261ea680456
dannee_v <dannee_v@MacBook-Victor>
2020-03-21 00:00:57 +0900

    Design: Created a prototype of the upgrade panel

---
+++ Space Shooter/Assets/Scripts/UI/TabButton.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+[RequireComponent(typeof(Image))]
+public class TabButton : MonoBehaviour, IPointerEnterHandler, IPointerClickHandler, IPointerExitHandler
+{
+    [SerializeField]
+    private TabGroup _tabGroup;
+
+    [SerializeField]
+    private GameObject _content;
+
+    [SerializeField]
+    private bool _isDefaultTab = false;
+
+    [HideInInspector]
+    public Image Background;
+
+    public GameObject Content { get { return _content; } }
+    public bool IsDefaultTab { get { return _isDefaultTab; } }
+
+    private void Awake()
+    {
+        Background = GetComponent<Image>();
+        _tabGroup.Suscribe(this);
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        _tabGroup.OnTabSelected(this);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        _tabGroup.OnTabEnter(this);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        _tabGroup.OnTabExit(this);
+    }
+}
---
+++ Space Shooter/Assets/Scripts/UI/TabGroup.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TabGroup : MonoBehaviour
+{
+    [SerializeField]
+    private Sprite _tabIdle;
+    [SerializeField]
+    private Sprite _tabHover;
+    [SerializeField]
+    private Sprite _tabActive;
+
+    [SerializeField]
+    private List<TabButton> _tabButtons;
+
+    [SerializeField]
+    private List<GameObject> _pages;
+
+    private TabButton _selectedTab;
+
+    private void Start()
+    {
+        // Set default tab
+        if (_tabButtons != null)
+        {
+            int index = _tabButtons.FindIndex(button => button.IsDefaultTab == true);
+            if (index != -1)
+                OnTabSelected(_tabButtons[index]);
+        }
+    }
+
+    public void Suscribe(TabButton button)
+    {
+        if (_tabButtons == null)
+        {
+            _tabButtons = new List<TabButton>();
+        }
+
+        _tabButtons.Add(button);
+    }
+
+    public void OnTabEnter(TabButton button)
+    {
+        ResetTabs();
+
+        if (_selectedTab == null || _selectedTab != button)
+        {
+            button.Background.sprite = _tabHover;
+        }
+    }
+
+    public void OnTabExit(TabButton button)
+    {
+        ResetTabs();
+    }
+
+    public void OnTabSelected(TabButton button)
+    {
+        _selectedTab = button;
+
+        ResetTabs();
+        button.Background.sprite = _tabActive;
+
+        int index = _pages.FindIndex(page => page.Equals(button.Content));
+        for (int i = 0; i < _pages.Count; i++)
+        {
+            if (i == index)
+                _pages[i].SetActive(true);
+            else
+                _pages[i].SetActive(false);
+        }
+    }
+
+    private void ResetTabs()
+    {
+        foreach (TabButton button in _tabButtons)
+        {
+            if (button != null && button == _selectedTab) continue;
+
+            button.Background.sprite = _tabIdle;
+        }
+    }
+}
