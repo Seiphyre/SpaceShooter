0289c40d622f79342afa0770a45ccfa2cb835157
dannee_v <dannee_v@MacBook-Victor>
2020-03-21 18:34:29 +0900

    Script & Prefab : Created a script to manage a progress bar with segments + Created a prefab of this progress bar, so it is reusable.

---
+++ Space Shooter/Assets/Scripts/UI/SegmentedProgressBar.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SegmentedProgressBar : MonoBehaviour
+{
+
+    // -- Attributes  --------------------------------------------------------
+
+    [SerializeField]
+    [Tooltip("This value should be equal to Segment raw image rectUV.W")]
+    private int _nbrOfSegments;
+
+    [SerializeField]
+    private Image _segmentMask;
+
+    [SerializeField]
+    private Image _segmentFilter;
+
+    private int _value;
+
+    private int _maxValue;
+
+
+
+    // -- Getters / Setters  --------------------------------------------------
+
+    public int Value { get { return _value; } }
+    public int MaxValue { get { return _maxValue; } }
+
+
+
+    // -- Functions -----------------------------------------------------------
+
+    private void Awake()
+    {
+        Init(_nbrOfSegments);
+        SetValue(0);
+    }
+
+    public void Init(int maxValue)
+    {
+        maxValue = Mathf.Clamp(maxValue, 0, _nbrOfSegments);
+        _maxValue = maxValue;
+
+        _segmentMask.fillAmount = (float)_maxValue / (float)_nbrOfSegments;
+    }
+
+    public void SetValue(int value)
+    {
+        value = Mathf.Clamp(value, 0, _maxValue);
+        _value = value;
+
+        _segmentFilter.fillAmount = 1f - _segmentMask.fillAmount + ((float)(_maxValue - _value) / (float)_nbrOfSegments);
+    }
+
+    public void AddValue(int amount = 1)
+    {
+        SetValue(_value + amount);
+    }
+
+    public void SubstractValue(int amount = 1)
+    {
+        SetValue(_value - amount);
+    }
+}
